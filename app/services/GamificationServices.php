9f0b1ac256468d711ac7b6fd4d6024d398fb0244
aboutuil41-glitch <aboutuil41@gmail.com>
2026-04-21 11:17:42 +0100

    Added Leveling System

--- app/services/GamificationServices.php
+++ app/services/GamificationServices.php
@@ -11,10 +11,29 @@ if($amount <= 0){
     return;
 }
 $user->xp += $amount;
+$user->level = $this->getLevel($user->xp);
 
 }
 
+public function passQuiz($user){
+    $this->awardXp($user, 30);
+}
 
+public function getLevel($xp){
+
+$level = 1;
+$requiredXp = 100;
+$totalNeeded = 0;
+
+while($xp >= $requiredXp + $totalNeeded)
+{
+    $totalNeeded += $requiredXp;
+    $requiredXp += 50;
+    $level ++;
+}
+
+return $level;
+}
 
 
 
